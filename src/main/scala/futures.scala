b3a9dd1623a3104e83f7d6f2b13af92ea1a01b33
David Y. Ross <dyross@gmail.com>
2012-05-02 00:21:09 -0700

    add akka and futures example

---
+++ src/main/scala/futures.scala
@@ -0,0 +1,48 @@
+object futures {
+
+    import akka.actor._
+    import akka.dispatch._
+    import akka.util.duration._
+
+    implicit val system = ActorSystem("foo")
+    implicit val ec = ExecutionContext.defaultExecutionContext
+
+    def a: Future[String] = Future { "a" }
+    def b: Future[Int] = Future { 2 }
+    def c: Future[List[Int]] = Future { Nil }
+
+    case class ABC(a: String, b: Int, c: List[Int])
+
+    // how to write a funciton to return an ABC from those above?
+
+    def blockingAndNotTypeSafe: ABC = {
+        val futureA = a
+        val futureB = b
+        val futureC = c
+
+        val bigFuture = Future sequence List(futureA, futureB, futureC)
+
+        val results = Await.result(bigFuture, 10 seconds)
+
+        ABC(
+            a = results(0).asInstanceOf[String], 
+            b = results(1).asInstanceOf[Int],
+            c = results(2).asInstanceOf[List[Int]])
+    }
+
+    def nonBlockingAndAmazing: Future[ABC] = {
+        val futureA = a
+        val futureB = b
+        val futureC = c
+
+        for (nowA <- futureA; nowB <- futureB; nowC <- futureC) yield ABC(nowA, nowB, nowC)
+    }
+
+    def onlyReturnIfBIs2: Future[Option[ABC]] = {
+        val abcFuture = nonBlockingAndAmazing
+
+        abcFuture map { abc =>
+            if (abc.b == 2) Some(abc) else None
+        }
+    }
+}
