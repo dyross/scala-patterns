1e298724a4748e8c5226626cc3e4d8e221741d6a
David Y. Ross <dyross@gmail.com>
2012-05-20 20:30:31 -0700

    add routing

--- src/main/scala/actor.scala
+++ src/main/scala/actor.scala
@@ -1,22 +1,32 @@
 import akka.actor._
+import akka.routing.RoundRobinRouter
 
 object actors {
 
     case object WritesOff
     case class WriteToCluster(cluster: String)
     case class Write(data: String)
+    case class DataAndCluster(data: String, cluster: String)
 
     class Master extends Actor {
 
         var clusterTarget: Option[String] = None
 
+        val writeRouter = context actorOf
+            (Props[Writer].withRouter(RoundRobinRouter(nrOfInstances = 5)))
+
         def receive = {
             case WritesOff               => clusterTarget = None
             case WriteToCluster(cluster) => clusterTarget = Some(cluster)
-            case Write(data)             => clusterTarget foreach (writeDataToCluster(data, _))
+            case Write(data)             => clusterTarget foreach (writeRouter ! DataAndCluster(data, _))
         }
+    }
 
-        def writeDataToCluster(data: String, cluster: String) { /* ... */ }
+    class Writer extends Actor {
+
+        def receive = {
+            case DataAndCluster(data: String, cluster: String) => // ...
+        }
 
     }
 
@@ -24,7 +34,7 @@ object actors {
         def watch(path: String)(callback: Option[String] => Unit) { /* ... */ }
     }
 
-    val actor = ActorSystem("system").actorOf(Props(classOf[Master]))
+    val actor = ActorSystem("system") actorOf Props[Master] 
 
     ZkClient.watch("/Status/writes") {
         _ match {
